2ad48152c8b63414ef92ed3f49e3f1b8729cb0ad
pdekyssky <p.dekyssky@gmail.com>
2023-06-05 09:58:01 +0200

    React day3 learned .map() .filter()

---
+++ react-day3/src/Data.jsx
@@ -0,0 +1,8 @@
+const Data = [
+    {id: 1, name: "vyvencit psa"},
+    {id: 2, name: "upratat kuchynu"},
+    {id: 3, name: "Nakupit"},
+    {id: 4, name: "Ist do fitka"},
+    {id: 5, name: "uvarit obed"}
+]
+export default Data
