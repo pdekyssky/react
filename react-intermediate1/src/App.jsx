756b10763380f715a2da1c725aa986102a4e3bbe
pdekyssky <p.dekyssky@gmail.com>
2023-06-16 13:47:06 +0200

    form react next make submit button onChange and useState

---
+++ react-intermediate1/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+
+const App = () => {
+  return (
+    <div>
+      <form>
+          <input type="text"  placeholder="name" className="userName"/>
+          <input type="submit"  value="Submit"/>
+      </form>
+    </div>
+  )
+}
+
+export default App
