6d10a341d9ba42c392bbea8edbcb3d9958843575
pdekyssky <p.dekyssky@gmail.com>
2023-06-18 16:21:27 +0200

    just trying something

--- react-intermediate1/src/App.jsx
+++ react-intermediate1/src/App.jsx
@@ -1,6 +1,37 @@
 import "./App.css"
 import { useState } from "react"
 
+//spread operator
+
+// join arrays together
+
+//const studentsGroup = ["Peter", "Tomas", "David"]
+//const allStundets = ["Harry", "Ron", ...studentsGroup, "Hermiona"]
+
+//console.log(allStundets);
+
+//function arguments
+//const number = [1, 2, 3]
+//const multipleNumbers = (a, b, c) => {
+  //console.log(a* b* c);
+//}
+//multipleNumbers(...number)
+
+
+//rest operator 
+
+//const sum = (...allNumbers) => {
+ // let result = 0
+  //for (const oneNumber of allNumbers) {
+    //result += oneNumber
+  //}
+  //return result
+ 
+//} 
+
+console.log(sum(1,2,15,10));
+
+
 const App = () => {
 
   const [inputValue, setInputValue] = useState("")
