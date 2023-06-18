b84c814f1a1aa2f88febd1d1b1de64d755721495
pdekyssky <p.dekyssky@gmail.com>
2023-06-18 15:53:39 +0200

    input form intermediate

--- react-intermediate1/src/App.jsx
+++ react-intermediate1/src/App.jsx
@@ -1,12 +1,45 @@
 import "./App.css"
+import { useState } from "react"
 
 const App = () => {
+
+  const [inputValue, setInputValue] = useState("")
+  const [names, setNames] = useState([])
+  console.log(names);
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+      if (inputValue) {
+        setNames( (names) => {
+          return [...names, inputValue]
+        })
+      } else {
+        console.log("No name");
+      }
+      setInputValue("")
+  }
+
   return (
     <div>
       <form>
-          <input type="text"  placeholder="name" className="userName"/>
-          <input type="submit"  value="Submit"/>
+          <input 
+            type="text"  
+            placeholder="name" 
+            className="userName"
+            value={inputValue}
+            onChange={(event) => setInputValue(event.target.value)}
+          />
+          <input 
+            type="submit"  
+            value="Submit" 
+            onClick={handleSubmit}
+          />
       </form>
+
+      {names.map( (oneName, index) => {
+        return <p className="item" key={index}>{oneName}</p>
+       
+      })}
     </div>
   )
 }
