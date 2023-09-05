602ff232d5c603e25693c023d0492ffa1878f468
pdekyssky <p.dekyssky@gmail.com>
2023-09-05 13:57:56 +0200

    finished searching input

--- input-search/src/App.jsx
+++ input-search/src/App.jsx
@@ -1,20 +1,38 @@
 import data from "./data"
 import {useState, useEffect} from "react"
+import "./App.css"
 
 
 const App = () => {
 
   const [searchText, setSearchText] = useState("")
   console.log(searchText);
+  const [filterMovies, setFilterMovies] = useState([])
 
-  return <>
+
+  /** filter all movies data 
+   * useEffect renders / run at the end of the component
+   * return only movies that includes search text
+   * setFilterMovies via useState hook to filterMovies so i can render them later in return
+   *  * */
+  useEffect( () => {
+    const filteredMovies = data.filter( (oneMovie) => {
+      return oneMovie.title.toLowerCase().includes(searchText.toLowerCase())
+    })
+    setFilterMovies(filteredMovies)
+  }, [searchText])
+  /*searchText is dependencz its mean it will start useEffect when searchText will chang*/
+
+  return <div className="movies-box">
+  {/**take value of input save it via setSearchText into searchText**/}
   <form>
     <input placeholder="search for movie" onChange={(e)=>setSearchText(e.target.value)}></input>
   </form>
-    <div>
-      {data.map( (oneMovie) =>{
+    <div className="all-movies">
+    {/*data.map i have change for filterMovies because i want to render only movies which has been filtered before*/}
+      {filterMovies.map( (oneMovie) =>{
          const {id, image, title, age, tags, description} = oneMovie
-         return <div key={id}>
+         return <div key={id} className="one-movie">
           <img src={image}></img>
           <h2>{title}</h2>
           <p>{age}</p>
@@ -23,7 +41,7 @@ const App = () => {
          </div>
       } )}
     </div>
-  </>
+  </div>
 
 
 
