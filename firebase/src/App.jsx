4a90990d3eb5462bf026cbe2008aa9913eaa63fc
pdekyssky <p.dekyssky@gmail.com>
2023-09-11 13:08:51 +0200

    learning firebase 8.5 next project try 10.3

--- firebase/src/App.jsx
+++ firebase/src/App.jsx
@@ -4,9 +4,44 @@ import "./App.css"
 
 const App = () => {
 
+  const [data, setData] = useState( [] )
+  const [error, setError] = useState("")
+
+  useEffect( () => {
+    projectFirestore.collection("movies").get().then( (snapshot) => {
+      //console.log(snapshot);
+      if (snapshot.empty) {
+        setError("Something went wrong try again later")
+      } else {
+        let result = []
+        snapshot.docs.forEach( (oneSnapshot) => {
+          result.push( {id: oneSnapshot.id, ...oneSnapshot.data()})
+        })
+        //console.log(result);
+        setData(result)
+      }
+    }).catch( (error) => {
+      setError(error.message)
+    })
+  }, [] )
+
+
  return (
-  <div>
-    <p>working</p>
+  <div className="all-movies">
+    {
+      error && <p>{error}</p>
+    }
+    {
+      data.map( (oneMovie) => {
+        const {id, title, length} = oneMovie
+        return (
+          <div key={id} className="one-movie">
+            <h2>{title}</h2>
+            <p>{length}</p>
+          </div>
+        )
+      })
+    }
   </div>
  )
 
